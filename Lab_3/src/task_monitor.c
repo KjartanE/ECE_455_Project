fd1f044ec77842eeaceb24b57874c1502e99eab3
lbare <levi.bare@gmail.com>
2024-03-25 19:19:00 -0700

    Implement task monitor

--- Lab_3/src/task_monitor.c
+++ Lab_3/src/task_monitor.c
@@ -1,4 +1,6 @@
 #include "task_monitor.h"
+#include "STM_32_RTOS_Config.h"
+#include "task_scheduler.h"
 /*
  * task_monitor.c
  *
@@ -10,7 +12,22 @@ void MonitorTask(void *pvParameters)
 {
     while (1)
     {
-        vTaskDelay(1000);
+        int activeCount = 0, completedCount = 0, overdueCount = 0;
+
+        dd_task_list *activeList = get_active_dd_task_list();
+        dd_task_list *completedList = get_completed_dd_task_list();
+        dd_task_list *overdueList = get_overdue_dd_task_list();
+
+        for (dd_task_list *list = activeList; list != NULL; list = list->next)
+            activeCount++;
+        for (dd_task_list *list = completedList; list != NULL; list = list->next)
+            completedCount++;
+
+        for (dd_task_list *list = overdueList; list != NULL; list = list->next)
+            overdueCount++;
+
+        printf("Active Tasks: %d, Completed Tasks: %d, Overdue Tasks: %d\n", activeCount, completedCount, overdueCount);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
-
